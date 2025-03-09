e50406d98c568fb0b792804bc091d72659f2f994
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2025-03-09 14:53:44 +0400

    Add profile fields to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'fullname',
+        'date_of_birth',
+        'phone',
+        'image',
+        'country',
+        'city',
     ];
 
     /**
