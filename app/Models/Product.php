30d7fb1d4224ad1f0e7e19f73c11ecd8444ba136
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2025-03-10 23:09:26 +0400

    Create models and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'discount',
+        'discount_start',
+        'discount_end',
+        'category_id',
+        'brand_id',
+    ];
+}
