b9fef1b9e2678ed5c8f90ea3b16abc44d4ceee5a
Ziyoviddin <univerziyo@email.com>
2025-04-13 01:41:51 +0400

    changing admin.ph

--- routes/admin.php
+++ routes/admin.php
@@ -27,10 +27,6 @@ Route::middleware('auth:admin')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
     Route::get('/products', IndexController::class)->name('product_list');
-    Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
-    Route::get('/brands', [BrandController::class, 'index'])->name('brand.index');
-
     Route::get('/product_create', CreateController::class)->name('product.create');
     Route::get('/product/{product}', ShowController::class)->name('product.show');
     Route::get('/product/{product}/edit', EditController::class)->name('product.edit');
@@ -38,18 +34,21 @@ Route::middleware('auth:admin')->group(function () {
     Route::patch('/product/{product}', UpdateController::class)->name('product.update');
     Route::delete('/product/{product}', DestroyController::class)->name('product.delete');
 
+    Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/new_category', [CategoryController::class, 'create'])->name('category.create');
     Route::get('/category/{category}', [CategoryController::class, 'show'])->name('category.show');
     Route::post('/new_category', [CategoryController::class, 'store'])->name('category.store');
-    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::delete('/category/{category}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
+    Route::get('/tags', [TagController::class, 'index'])->name('tag.index');
     Route::get('/new_tag', [TagController::class, 'create'])->name('tag.create');
     Route::get('/tag/{tag}', [TagController::class, 'show'])->name('tag.show');
     Route::post('/new_tag', [TagController::class, 'store'])->name('tag.store');
-    Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tag.destroy');
+    Route::delete('/tag/{tag}', [TagController::class, 'destroy'])->name('tag.destroy');
 
+    Route::get('/brands', [BrandController::class, 'index'])->name('brand.index');
     Route::get('/new_brand', [BrandController::class, 'create'])->name('brand.create');
     Route::get('/brand/{brand}', [BrandController::class, 'show'])->name('brand.show');
     Route::post('/new_brand', [BrandController::class, 'store'])->name('brand.store');
-    Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->name('brand.destroy');
+    Route::delete('/brand/{brand}', [BrandController::class, 'destroy'])->name('brand.destroy');
 });
